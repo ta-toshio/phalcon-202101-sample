930695c79e278909311f4cad77cedf170bbf0790
ta-toshio <ta.toshio.info@gmail.com>
2021-09-19 14:03:01 +0900

    Add migration

--- backend/my-site/app/config/config.php
+++ backend/my-site/app/config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+use Phalcon\Config;
+
 /*
  * Modified: prepend directory path of current file, because of this file own different ENV under between Apache and command line.
  * NOTE: please remove this comment.
@@ -7,13 +9,13 @@
 defined('BASE_PATH') || define('BASE_PATH', getenv('BASE_PATH') ?: realpath(dirname(__FILE__) . '/../..'));
 defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 
-return new \Phalcon\Config([
+return new Config([
     'database' => [
         'adapter'     => 'Mysql',
-        'host'        => 'localhost',
-        'username'    => 'root',
-        'password'    => '',
-        'dbname'      => 'test',
+        'host'        => 'db',
+        'username'    => 'myuser',
+        'password'    => 'secret',
+        'dbname'      => 'mydatabase',
         'charset'     => 'utf8',
     ],
     'application' => [
---
+++ backend/my-site/app/migrations/1.0.0/users.php
@@ -0,0 +1,89 @@
+<?php
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Exception;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Migrations\Mvc\Model\Migration;
+
+/**
+ * Class UsersMigration_100
+ */
+class UsersMigration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function morph(): void
+    {
+        $this->morphTable('users', [
+            'columns' => [
+                new Column(
+                    'id',
+                    [
+                        'type' => Column::TYPE_BIGINTEGER,
+                        'unsigned' => true,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 1,
+                        'first' => true
+                    ]
+                ),
+                new Column(
+                    'name',
+                    [
+                        'type' => Column::TYPE_VARCHAR,
+                        'default' => "",
+                        'notNull' => true,
+                        'size' => 255,
+                        'after' => 'id'
+                    ]
+                ),
+            ],
+            'indexes' => [
+                new Index('PRIMARY', ['id'], 'PRIMARY'),
+                new Index('id', ['id'], 'UNIQUE'),
+            ],
+            'options' => [
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '1',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'utf8mb4_0900_ai_ci',
+            ],
+        ]);
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+    }
+
+    /**
+     * This method is called after the table was created
+     *
+     * @return void
+     */
+    public function afterCreateTable(): void
+    {
+        $this->batchInsert('users', [
+            'id',
+            'name',
+        ]);
+    }
+}
